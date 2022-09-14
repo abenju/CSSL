0bcccfba49d5510d66b23f27b6fd32eafec3ca6c
Aduen Benjumea <aduen.j.benjumea@gmail.com>
2022-09-14 11:06:05 +0100

    improved headounts from heatmaps and changed format output

--- heatmap_to_number.py
+++ heatmap_to_number.py
@@ -1,50 +1,16 @@
-import cv2
-import pandas as pd
 import numpy as np
 
 import os
-import json
-
-import tqdm
-
-def csv_to_img(path):
-    data = pd.read_csv(path)
-    print(data.shape)
-    img = data.to_numpy()
-    #norm = np.linalg.norm(img)
-    img = img / img.max()
-    img = img * 255
-    img = img.astype(np.uint8)
-    return img
+import glob
 
 
-def img_to_num(img):
-    th, threshold = cv2.threshold(img, 170, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY)
-    cnts = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]
+if __name__ == "__main__":
+    path = os.getenv("GT_DIR") + "*.csv"
 
-    print("nums: {}".format(len(cnts)))
+    counts = []
+    for f in glob.glob(path):
+        data = np.genfromtxt(f, delimiter=',')
+        count = np.sum(data)
+        counts.append(count)
 
-    #cv2.imshow("image", img)
-    #cv2.waitKey(0)
-
-    # cv2.drawContours(np.vstack((img, np.zeros(img.size), np.zeros(img.size))), np.array(cnts), -1, (0, 255, 0), 1,
-    #                  cv2.LINE_AA)
-    # cv2.imwrite("dst.png", img)
-
-    return len(cnts)
-
-
-def convert_all(dir):
-    result = {}
-    for root, dirs, files in os.walk(dir):
-        for file in files:
-            path = os.path.join(root, file)
-            img = csv_to_img(path)
-            num = img_to_num(img)
-            result[file] = num
-    return result
-
-
-def write_json(numbers, path):
-    with open(os.path.join(path, "count.json"), "w", encoding="utf-8") as file:
-        json.dump(numbers, file)
+    np.savetxt("counts.csv", counts, delimiter=",", fmt="%0.f")
