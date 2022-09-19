56ae668b8951b4e138886ebe5d91f0e7f59aab05
Aduen Benjumea <aduen.j.benjumea@gmail.com>
2022-09-19 13:42:42 +0100

    Expanded dataloader functionality

--- data_loader.py
+++ data_loader.py
@@ -6,30 +6,18 @@ import numpy as np
 import os
 import json
 from PIL import Image
+import glob
 
 
-class MallDataSet(Dataset):
-    def __init__(self):
-        samples = 0
-        feature_size = 512
-        gt_size = 480 * 640  # (479, 640)
+class MallDataset(Dataset):
 
-        preprocess = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
+    def __init__(self, preprocess, sets, is_map=False):
+        root_path = os.getenv("DATA_ROOT")
 
-        csv_path = r"D:\Documents\mscProject\aduen\counts.csv"
-
-        # features_dir = r'D:\Documents\mscProject\aduen\data\features\Feature_files'
-        # features_files = next(os.walk(features_dir))[2]
-        # features_paths = [os.path.join(features_dir, x) for x in features_files]
-
-        input_dir = r'D:\Documents\mscProject\IJCAI_2021_Continual_Crowd_Counting_Challenge\data\mall\train\input'
-        input_files = next(os.walk(input_dir))[2]
-        input_paths = [os.path.join(input_dir, x) for x in input_files]
+        input_paths = []
+        for s in sets:
+            path = fr"{root_path}\{s}\train\input\*.jpg"
+            input_paths += glob.glob(path)
 
         imgs = []
         for path in input_paths:
@@ -40,20 +28,28 @@ class MallDataSet(Dataset):
         samples = len(imgs)
         imgs = torch.stack(imgs)
 
-        gts = np.zeros((samples, 1))
-        data = np.genfromtxt(csv_path, delimiter=',')
-
-        for i, x in enumerate(data):
-            if i < samples:
-                gts[i] = x
-        gts = gts
-
         self.x = imgs
-        self.y = torch.from_numpy(gts)
         self.n_samples = samples
+
+        if is_map:
+            pass  # TODO: include density map import
+        else:
+            gt_paths = []
+            for s in sets:
+                path = fr"{root_path}\{s}\train\gt\*.jpg"
+                gt_paths += glob.glob(path)
+
+            gts = np.zeros((samples, 1))
+            for i, f in enumerate(gt_paths):
+                data = np.genfromtxt(f, delimiter=',')
+                count = np.sum(data)
+                gts[i, 0] = count
+
+        self.y = torch.from_numpy(gts)
 
     def __getitem__(self, item):
         return self.x[item], self.y[item]
 
     def __len__(self):
         return self.n_samples
+
