bc6224a6c861f97fea7645431c6df39d9430db33
Aduen Benjumea <aduen.j.benjumea@gmail.com>
2022-09-30 00:31:04 +0100

    Visual models from scratch

---
+++ models/models.py
@@ -0,0 +1,166 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import math
+
+from modules import ResNetBottleNeck, ResNetBlock, Conv, C3, SPPF, Concat
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers):
+        super(ResNet, self).__init__()
+
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+        # to be replaced by custom head
+        # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        # self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, in_channels, layer_n, stride=2):
+        downsample = None
+        if stride != 1 or self.inplanes != self.in_planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, in_channels * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(in_channels * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.in_planes, in_channels, stride, downsample))
+
+        self.in_planes = in_channels * block.expansion
+        for _ in range(1, layer_n):
+            layers.append(block(self.in_planes, in_channels))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.max_pool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        # x = self.avgpool(x)
+        # x = x.reshape(x.shape[0], -1)
+        # x = self.fc(x)
+
+        return x
+
+
+class ResNet18(nn.Module):
+    def __init__(self, header, **kwargs):
+        super(ResNet18, self).__init__()
+        self.model = ResNet(ResNetBlock, [2, 2, 2, 2])
+        self.header = header(**kwargs)
+
+    def forward(self, x):
+        x = self.model(x)
+        x = self.header(x)
+
+        return x
+
+
+class ResNet50(nn.Module):
+    def __init__(self, header, **kwargs):
+        super(ResNet50, self).__init__()
+        self.model = ResNet(ResNetBottleNeck, [3, 4, 6, 3])
+        self.header = header(**kwargs)
+
+    def forward(self, x):
+        x = self.model(x)
+        x = self.header(x)
+
+        return x
+
+
+def make_divisible(x, divisor):
+    # Returns nearest x divisible by divisor
+    if isinstance(divisor, torch.Tensor):
+        divisor = int(divisor.max())  # to int
+    return math.ceil(x / divisor) * divisor
+
+
+class YOLOv5S(nn.Module):
+    def __init__(self, header, **kwargs):
+        super(YOLOv5S, self).__init__()
+        self.conv_1 = Conv(3, 32, 6, 2, 2)
+        self.conv_2 = Conv(32, 64, 3, 2)
+        self.c3_1 = C3(64, 64, 1)
+        self.conv_3 = Conv(64, 128, 3, 2)
+        self.c3_2 = C3(128, 128, 2)  #
+        self.conv_4 = Conv(128, 256, 3, 2)
+        self.c3_3 = C3(256, 256, 3)  #
+        self.conv_5 = Conv(256, 512, 3, 2)
+        self.c3_4 = C3(512, 512, 1)
+        self.sppf = SPPF(512, 512, 5)
+
+        self.conv_6 = Conv(512, 256, 1, 1)  #
+        self.up_1 = nn.Upsample(None, 2, 'nearest')
+        self.concat_1 = Concat(1)
+        self.c3_5 = C3(512, 256, 1, False)
+
+        self.conv_7 = Conv(256, 128, 1, 1)  #
+        self.up_2 = nn.Upsample(None, 2, 'nearest')
+        self.concat_2 = Concat(1)
+        self.c3_6 = C3(256, 128, 1, False)
+
+        self.conv_8 = Conv(128, 128, 3, 2)
+        self.concat_3 = Concat(1)
+        self.c3_7 = C3(256, 256, 1, False)
+
+        self.conv_9 = Conv(256, 256, 3, 2)
+        self.concat_4 = Concat(1)
+        self.c3_8 = C3(512, 512, 1, False)
+
+        self.header = header(**kwargs)
+
+    def forward(self, x):
+        x = self.conv_1(x)
+        x = self.conv_2(x)
+        x = self.c3_1(x)
+        x = self.conv_3(x)
+        x = self.c3_2(x)
+        x_4 = x.clone()
+        x = self.conv_4(x)
+        x = self.c3_3(x)
+        x_6 = x.clone()
+        x = self.conv_5(x)
+        x = self.c3_4(x)
+        x = self.sppf(x)
+
+        x = self.conv_6(x)
+        x_10 = x.clone()
+        x = self.up_1(x)
+        x = self.concat_1([x, x_6])
+        x = self.c3_5(x)
+
+        x = self.conv_7(x)
+        x_14 = x.clone()
+        x = self.up_2(x)
+        x = self.concat_2([x, x_4])
+        x = self.c3_6(x)
+
+        x = self.conv_8(x)
+        x = self.concat_3([x, x_14])
+        x = self.c3_7(x)
+
+        x = self.conv_9(x)
+        x = self.concat_4([x, x_10])
+        x = self.c3_8(x)
+
+        x = self.header(x)
+
+        return x
---
+++ models/modules.py
@@ -0,0 +1,172 @@
+import torch
+import torch.nn as  nn
+import torch.nn.functional as F
+
+import math
+import warnings
+
+
+class CrowdCountHeader(nn.Module):
+    def __init__(self, in_features):
+        super(CrowdCountHeader, self).__init__()
+        self.avg = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(in_features, 1)
+
+    def forward(self, x):
+        x = self.avg(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+
+class DensityMapHeader(nn.Module):
+    pass  # TODO:
+
+
+class ResNetBottleNeck(nn.Module):
+    def __init__(self, in_channels, out_channels, i_downsample=None, stride=1):
+        super(ResNetBottleNeck, self).__init__()
+
+        self.expansion = 4
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.batch_norm1 = nn.BatchNorm2d(out_channels)
+
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.batch_norm2 = nn.BatchNorm2d(out_channels)
+
+        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1, padding=0)
+        self.batch_norm3 = nn.BatchNorm2d(out_channels * self.expansion)
+
+        self.i_downsample = i_downsample
+        self.stride = stride
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        identity = x.clone()
+        x = self.relu(self.batch_norm1(self.conv1(x)))
+
+        x = self.relu(self.batch_norm2(self.conv2(x)))
+
+        x = self.conv3(x)
+        x = self.batch_norm3(x)
+
+        if self.i_downsample is not None:
+            identity = self.i_downsample(identity)
+        x += identity
+        x = self.relu(x)
+
+        return x
+
+
+class ResNetBlock(nn.Module):
+
+    def __init__(self, in_channels, out_channels, i_downsample=None, stride=1):
+        super(ResNetBlock, self).__init__()
+
+        self.expansion = 1
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=stride, bias=False)
+        self.batch_norm1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, stride=stride, bias=False)
+        self.batch_norm2 = nn.BatchNorm2d(out_channels)
+
+        self.i_downsample = i_downsample
+        self.stride = stride
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        identity = x.clone()
+
+        x = self.relu(self.batch_norm2(self.conv1(x)))
+        x = self.batch_norm2(self.conv2(x))
+
+        if self.i_downsample is not None:
+            identity = self.i_downsample(identity)
+        print(x.shape)
+        print(identity.shape)
+        x += identity
+        x = self.relu(x)
+        return x
+
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    # Pad to 'same' shape outputs
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    # Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        return self.act(self.conv(x))
+
+
+class Bottleneck(nn.Module):
+    # Standard bottleneck
+    def __init__(self, c1, c2, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_, c2, 3, 1, g=g)
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+
+class C3(nn.Module):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
+
+    def forward(self, x):
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
+
+
+class SPPF(nn.Module):
+    # Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher
+    def __init__(self, c1, c2, k=5):  # equivalent to SPP(k=(5, 9, 13))
+        super().__init__()
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_ * 4, c2, 1, 1)
+        self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
+
+    def forward(self, x):
+        x = self.cv1(x)
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')  # suppress torch 1.9.0 max_pool2d() warning
+            y1 = self.m(x)
+            y2 = self.m(y1)
+            return self.cv2(torch.cat((x, y1, y2, self.m(y2)), 1))
+
+
+class Concat(nn.Module):
+    # Concatenate a list of tensors along dimension
+    def __init__(self, dimension=1):
+        super().__init__()
+        self.d = dimension
+
+    def forward(self, x):
+        return torch.cat(x, self.d)
