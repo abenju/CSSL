23b3dc46fb24fcbd50cfd3dc39c9fbf3620407b2
Aduen Benjumea <aduen.j.benjumea@gmail.com>
2022-10-02 00:16:20 +0100

    cleaned model builder

--- model_builder.py
+++ model_builder.py
@@ -1,22 +1,26 @@
 import torch
 import torchvision
 
+from models.models import *
+from models.modules import CrowdCountHeader, DensityMapHeader
+
 
 def build_vis_model(model_name, density_map):
-    if density_map:
-        pass  # TODO
+    header = DensityMapHeader if density_map else CrowdCountHeader
+
+    if model_name == "Resnet_18":  # ["Resnet_18", "Resnet_50", "YOLO5S", "FCNresnet50"]
+        par = None if density_map else {"in_features": 512}
+        model = ResNet18(header, **par)
+    elif model_name == "Resnet_50":
+        par = None if density_map else {"in_features": 2048}
+        model = ResNet50(header, **par)
+    elif model_name == "YOLO5S":
+        par = None if density_map else {"in_features": 512}
+        model = YOLOv5S(header, **par)
+    elif model_name == "Unet":
+        par = None if density_map else {"in_features": 64}
+        model = UNet(header, **par)
     else:
-        if model_name == "Resnet_18":  # ["Resnet_18", "Resnet_50", "YOLO5S", "FCNresnet50"]
-            model = torchvision.models.resnet18(pretrained=True)
-            model.fc = torch.nn.Linear(512, 1)
-        elif model_name == "Resnet_50":
-            model = torchvision.models.resnet50(pretrained=True)
-            model.fc = torch.nn.Linear(2048, 1)
-        elif model_name == "YOLO5S":
-            pass
-        elif model_name == "FCNresnet50":
-            pass
-        else:
-            raise Exception("Unexpected model")
+        raise Exception("Unexpected model")
 
     return model
